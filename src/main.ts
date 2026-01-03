a6c20348a479ebd3e44ccae951cb701c724e4be8
Danilo <chessteacher87@gmail.com>
2026-01-03 19:07:03 +0100

    Fix index.html to use app root component; fix calendar grid lines

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import {DayGrid} from './app/pages/day-grid/day-grid';
+import {App} from './app/app';
+import {provideRouter} from '@angular/router';
+import {routes} from './app/app.routes';
 
-
-bootstrapApplication(DayGrid, {
-  providers: [provideHttpClient()],
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+  ],
 }).catch(err => console.error(err));
